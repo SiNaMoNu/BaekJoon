835a48b08afebdb1c85d588a6f2e10275f7d62d0
Park Seungbeom <81613308+SiNaMoNu@users.noreply.github.com>
2021-08-18 21:05:09 +0900

    Create 11654.java

---
+++ Java/11654.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        char hangeul = sc.next().charAt(0);
+        int C = (int)hangeul;
+        
+        System.out.println(C);
+	}
+}
