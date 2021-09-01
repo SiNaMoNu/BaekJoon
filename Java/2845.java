7781e388ed07f6fd26e1bffa3ae0882d38a4626d
Park Seungbeom <81613308+SiNaMoNu@users.noreply.github.com>
2021-09-01 20:14:50 +0900

    Create 2845.java

---
+++ Java/2845.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int N1 = sc.nextInt();
+        int N2 = sc.nextInt();
+        int N3 = sc.nextInt();
+        int N4 = sc.nextInt();
+        int N5 = sc.nextInt();
+        int N6 = sc.nextInt();
+        int N7 = sc.nextInt();
+
+        System.out.println((N3 - (N1 * N2)) + " " + (N4 - (N1 * N2)) + " " + (N5 - (N1 * N2)) + " " + (N6 - (N1 * N2)) + " " + (N7 - (N1 * N2)));
+	}
+}
